2df9ce2423dc369d1a30ea4833271f62a838e0a0
sergey <sergey>
2018-05-03 09:36:45 +0300

    orders9

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,30 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Orders</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Orders</h1>
+          <v-list two-line subheader>
+          <v-list-tile
+            avatar
+            v-for="order of orders"
+            :key="order.id"
+          >
+            <v-list-tile-action>
+              <v-checkbox
+                color="success"
+                :input-value="order.done"
+                @change="markDone(order)"
+              ></v-checkbox>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ order.name }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ order.phone }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn class="primary" :to="'/ad/' + order.id">Open</v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,7 +34,20 @@
   export default {
     data(){
       return {
-
+        orders: [
+          {
+            id: '123',
+            name: 'Sergey',
+            phone: '8 911 009 77 88',
+            adId: '124',
+            done: false
+          }
+        ]
+      }
+    },
+    methods: {
+      markDone(order){
+        order.done = true;
       }
     }
   }
