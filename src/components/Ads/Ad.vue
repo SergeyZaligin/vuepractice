59c040ec0b1953972d47549566c54aba67decdae
sergey <sergey>
2018-05-04 10:36:30 +0300

    end layout

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,23 @@
   <v-container>
     <v-layout row>
       <v-flex xs12>
-        <h1>Ad</h1>
+        <v-card>
+          <v-card-media
+            src="https://vuetifyjs.com/static/doc-images/carousel/sky.jpg"
+            height="300"
+          ></v-card-media>
+          <v-card-text>
+            <h2 class="text--primary">title</h2>
+            <p>
+              lorem
+            </p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="warning">Edit</v-btn>
+            <v-btn class="success">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
